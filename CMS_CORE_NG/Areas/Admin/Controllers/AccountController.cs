6b0b9f181a91567f9babdad7c2d4ff4c7ce5d129
Sabrinegl <sabrineggll@gmail.com>
2022-05-28 13:39:56 +0100

    first commit

---
+++ CMS_CORE_NG/Areas/Admin/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CMS_CORE_NG.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+    }
+}
